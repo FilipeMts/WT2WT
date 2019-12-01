e3d19e9ae08039574bb65fae25e1ae46368a2021
FilipeMts <jfgmatos@outlook.com>
2019-12-01 08:00:42 +0000

    social login updated

--- passport-config.js
+++ passport-config.js
@@ -166,7 +166,7 @@ passport.use(
   new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/redirect"
+      callbackURL: "https://wt2wtc.herokuapp.com//auth/google/redirect"
     },
     function (accessToken, refreshToken, profile, callback) {
       //console.log(profile._json.sub)
